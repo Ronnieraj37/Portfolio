b4fc9fee314e8096f312a36352d2e47cb2995da9
Ronnieraj37 <101713157+Ronnieraj37@users.noreply.github.com>
2024-01-24 22:49:06 +0530

    adding

---
+++ src/data/constants.js
@@ -0,0 +1,245 @@
+export const Bio = {
+  name: "Roshan Raj Singh",
+  roles: [
+    "Blockchain Developer",
+    "Web 3.0 Developer",
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "Blockchain developer proficient in C++, JavaScript, Flutter, Dart, and Solidity. Skilled in developing decentralized applications and smart contracts on various blockchain platforms, including Solidity, Hardhat, ReactJS, and Ethers. Experienced with multiple blockchain protocols.",
+  github: "https://github.com/Ronnieraj37",
+  resume:
+    "https://drive.google.com/file/d/1mCCuI2HjJE_Tr7JUDQaYvXEL3QDdxnYe/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/roshan-singh-rajput/",
+  whatsApp: "https://wa.me/8919285030",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: 'Tailwind CSS',
+        image: "https://pbs.twimg.com/profile_images/1730334391501488129/G0R0sjHH_400x400.jpg",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Solidity",
+        image: "https://pbs.twimg.com/profile_images/1240311693177167892/-5a2yoQp_400x400.jpg",
+      },
+      {
+        name: "Polybase DB",
+        image: "https://pbs.twimg.com/profile_images/1597289438513610758/1RnEmeJE_400x400.jpg",
+      },
+      {
+        name: "Diamond Structure",
+        image: "https://eips.ethereum.org/assets/eip-2535/diamond.svg",
+      },
+      {
+        name: "Hyperledger Fabric",
+        image: "https://www.hyperledger.org/hubfs/Imported_Blog_Media/Hyperledger_Fabric_Logo_Color-1-1024x286.png",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Protocols",
+    skills: [
+
+      {
+        name: "Chainlink CCIP & Automated Contracts",
+        image:
+          "https://pbs.twimg.com/profile_images/1030475757892579334/qvSHhRyC_400x400.jpg",
+      },
+      {
+        name: "IPFS",
+        image: "https://ipfs.tech/_nuxt/ipfs-logo.a313bcee.svg",
+      },
+      {
+        name: "LivePeer",
+        image:
+          "https://pbs.twimg.com/profile_images/1725317650169860096/TReyfYDk_400x400.jpg",
+      },
+      {
+        name: "Lens Protocol",
+        image:
+          "https://pbs.twimg.com/profile_images/1747266965570453504/ulWRrdH__400x400.jpg",
+      },
+      {
+        name: "Push Protocol",
+        image:
+          "https://pbs.twimg.com/profile_images/1616110831975931909/5tUeoUR__400x400.png",
+      },
+      {
+        name: "Polygon ZK Proof",
+        image:
+          "https://pbs.twimg.com/profile_images/1624229555333373952/JXGKFcO__400x400.jpg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://pbs.twimg.com/profile_images/1652878800311427073/j0-3owJd_400x400.jpg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Hardhat",
+        image: "https://pbs.twimg.com/profile_images/1317925773425168384/XQkaoFRg_400x400.jpg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQGD88-Rrn-iQg/company-logo_200_200/0/1697875306075?e=1706745600&v=beta&t=-ki0k8vpGXKaD9CPTJu7MbdShwlIg-xjE7seKsnbAss",
+    role: "Full Stack Web 3.0 Intern",
+    company: "Kuvaka Tech",
+    date: "Sep 2023 - Dec 2023",
+    desc: "Built a Full Stack Blockchain Model for the Government of Qatar to revolutionize money flow and tender system management through decentralization. Developed a Hyperledger Fabric solution for India's Leading Farming Company's supply chain system.",
+    skills: [
+      "Solidity",
+      "EthersJs",
+      "Hardhat",
+      "ReactJs",
+      "NextJs",
+      "TailwindCSS",
+      "ERC-721",
+      "Fabric Chaincode",
+      "Hyperledger Fabric",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4D0BAQFDXGNQT2FnVw/company-logo_200_200/0/1620050620599?e=2147483647&v=beta&t=ahiy7BGdXjP2v1ewGhDTR0LOL21SV5Ng18A9XzW9oxQ",
+    role: "Blockchain Intern",
+    company: "GetBoarded",
+    date: "May 2023 - Aug 2023",
+    desc: "Created an NFT/Token Gated Service using ERC1155 standard. Successfully integrated LivePeer, Ocean, and Lens Protocol technologies, enhancing the functionality and capabilities of existing applications.",
+    skills: [
+      "Solidity",
+      "EthersJs",
+      "Hardhat",
+      "ReactJs",
+      "VueJs",
+      "TailwindCSS",
+      "ERC-1155",
+      "Lens Protocol",
+      "Livepeer",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/4/4a/Indian_Institute_of_Information_Technology%2C_Design_and_Manufacturing%2C_Kancheepuram_logo.png",
+    school: "Indian Institute of Information Technology, Chennai, Tamil Nadu",
+    date: "Nov 2021 - July 2025",
+    grade: "8.0 CGPA",
+    degree: "BTech, Electronics and Communication Engineering",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "File Guardian",
+    description:
+      "Developed File Guardian, a blockchain based data protection system allowing controlled data access, revocable permissions and anti-forgery measures using deep learning",
+    image:
+      "https://i.postimg.cc/J4TNGhLw/c3389bb7-7d79-46a6-acdc-18e72db325e8.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    github: "https://github.com/Ronnieraj37/Group-Project",
+    webapp: "https://podstream.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Vendork",
+    description:
+      "Created a privacy focused blockchain powered chat application with encrypted message transaction, IPFS file storage, AI-driven message responses. It was built on ERC-2535 Diamond structure allowing it to be upgradeable but also saving user data",
+    image:
+      "https://i.postimg.cc/63wcC1bF/image-2.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    github: "https://github.com/Ronnieraj37/Mantle",
+    webapp: "https://mantle37.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Drive 3.0",
+    description:
+      "Established a decentralized data storage platform similar to Google Drive, leveraging Solidity, Hardhat, Ethers and IPFS.",
+    image:
+      "https://i.postimg.cc/FK0GdQpj/image.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    github: "https://github.com/Ronnieraj37/G-Drive-3.0",
+    webapp: "https://mantle37.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Smart Voting",
+    description:
+      "A decentralized mobile app made using Flutter, Dart, Solidity and web3 api for fair election. This app enables transparent and secure voting for anyone ensuring the integrity of the electoral process.",
+    image:
+      "https://i.postimg.cc/PqBx7gnF/27198.jpg",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    github: "https://github.com/Ronnieraj37/Smart-Voting",
+  },
+  {
+    id: 4,
+    title: "Iot - Blockchain",
+    description:
+      "Integrating IOT with Blockchain making Channels for Secure data flow",
+    image:
+      "https://i.postimg.cc/FHMDHKyr/image-1.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    github: "https://github.com/Ronnieraj37/Iot-Blockchain",
+  },
+];
+
